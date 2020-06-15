4810bc11a524de9d95d4a74a9a7f7f1bd437eb63
Oded Welgreen <oded.welgreen@gmail.com>
2020-06-15 01:18:25 -0500

    add ExerciseI1

--- p5/src/sketches/NatureOfCode/ExerciseI1.ts
+++ p5/src/sketches/NatureOfCode/ExerciseI1.ts
@@ -1,12 +1,35 @@
 import Sketch from "../Sketch";
+import p5 from "p5";
 
+class Walker {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  display(p: p5) {
+    p.stroke(0);
+    p.point(this.x, this.y);
+  }
+  step(p: p5) {
+    const stepx = p.min(p.round(p.random(-1, 1.5)), 1);
+    const stepy = p.min(p.round(p.random(-1, 1.5)), 1);
+
+    this.x += stepx;
+    this.y += stepy;
+  }
+}
 export class ExerciseI1 extends Sketch {
-  backgroundColor = 0;
+  backgroundColor = 255;
+  w!: Walker;
 
   setup() {
     this.p.createCanvas(this.width, this.height);
+    this.w = new Walker(this.width / 2, this.height / 2);
   }
   draw() {
-    this.p.background(this.backgroundColor);
+    this.w.step(this.p);
+    this.w.display(this.p);
   }
 }
---
+++ p5/src/sketches/NatureOfCode/RandomDistribution.ts
@@ -0,0 +1,29 @@
+import Sketch from "../Sketch";
+
+export class RandomDistribution extends Sketch {
+  backgroundColor = 255;
+  randomCounts: number[] = new Array(20).fill(0);
+
+  setup() {
+    this.p.createCanvas(this.width, this.height);
+  }
+  draw() {
+    this.p.background(255);
+    const index = this.p.round(this.p.random(this.randomCounts.length - 1));
+    this.randomCounts[index]++;
+    this.p.stroke(0);
+    this.p.fill(175);
+    const w = this.width / this.randomCounts.length;
+
+    // Graphing the results
+
+    for (let x = 0; x < this.randomCounts.length; x++) {
+      this.p.rect(
+        x * w,
+        this.height - this.randomCounts[x],
+        w - 1,
+        this.randomCounts[x]
+      );
+    }
+  }
+}
