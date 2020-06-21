846f4918a8e4ec1c5ddbd7b58a0bc788bfb41427
Oded Welgreen <oded.welgreen@gmail.com>
2020-06-21 18:15:44 -0500

    add sound to exercise I-4

--- p5/src/sketches/NatureOfCode/ExerciseI4.ts
+++ p5/src/sketches/NatureOfCode/ExerciseI4.ts
@@ -1,22 +1,32 @@
 import Sketch from "../Sketch";
-import { Color } from "p5";
+import "p5/lib/addons/p5.sound";
+import { Color, SoundFile } from "p5";
 type Circle = {
   x: number;
   y: number;
   size: number;
   color: Color;
 };
-const CIRCLE_SIZE = 30;
+const CIRCLE_SIZE = 40;
+const MIN_TIME_BETWEEN_CIRCLES = 50;
 let timeToNextCircle = 1000;
+
 let elapsed = timeToNextCircle;
 let lastFrameTime = 0;
 let ratio = 0.9;
+let levelRatio = 0.95;
 let sd: number;
+let file: SoundFile;
+let currentLevel = 1;
+
 const circles: Circle[] = [];
 export class ExerciseI4 extends Sketch {
+  preload() {
+    file = this.p.loadSound(require("./assets/tom.wav"));
+  }
   setup() {
     const p = this.p;
-    sd = p.min(this.w, this.h) / 8;
+    sd = p.min(this.w, this.h) / 10;
     p.createCanvas(this.w, this.h);
     p.background(240);
   }
@@ -25,7 +35,11 @@ export class ExerciseI4 extends Sketch {
     const currentFrameTime = performance.now();
     elapsed -= currentFrameTime - lastFrameTime;
     if (elapsed <= 0) {
-      if (timeToNextCircle > 1) timeToNextCircle *= ratio;
+      file.play(0, p.random(0.5, 1.5), currentLevel, 0, file.duration());
+      if (timeToNextCircle > MIN_TIME_BETWEEN_CIRCLES) {
+        timeToNextCircle *= ratio;
+        currentLevel *= levelRatio;
+      }
       elapsed = timeToNextCircle;
       circles.push({
         x: p.randomGaussian(this.center.x, sd),
