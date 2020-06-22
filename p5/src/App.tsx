783840d54d27c7b91c350ff4739a388c2c9fe59e
Oded Welgreen <oded.welgreen@gmail.com>
2020-06-21 20:47:17 -0500

    rename

--- p5/src/App.tsx
+++ p5/src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { ExerciseI1 } from "./sketches/NatureOfCode/ExerciseI1";
-import { ExerciseI4 } from "./sketches/NatureOfCode/ExerciseI4";
+import { ExerciseI1 } from "./sketches/NatureOfCode/Exercise-I-1";
+import { ExerciseI4 } from "./sketches/NatureOfCode/Exercise-I-4";
 function App() {
   useEffect(() => {
     // new ExerciseI1().create();
-    new ExerciseI4().create();
+    // new ExerciseI4().create();
   }, []);
   return <div id="container"></div>;
 }
