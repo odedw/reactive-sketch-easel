fd096aaf19af635e754bac1dbcd9d0cb70391aed
Oded Welgreen <oded.welgreen@gmail.com>
2020-06-27 23:08:43 -0500

    remove commented code

--- shared/src/logger.ts
+++ shared/src/logger.ts
@@ -1,21 +1,4 @@
 import * as logger from "loglevel";
-
-// const levelNames = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "SILENT"];
-// var originalFactory = logger.methodFactory;
-//@ts-ignore
-// logger.methodFactory = function (methodName, logLevel, loggerName) {
-//   var rawMethod = originalFactory(methodName, logLevel, loggerName);
-
-//   return function () {
-//     var messages = [
-//       `${levelNames[logLevel]} | ${new Date().toLocaleTimeString()} | `,
-//     ];
-//     for (var i = 0; i < arguments.length; i++) {
-//       messages.push(arguments[i]);
-//     }
-//     rawMethod.apply(undefined, messages);
-//   };
-// };
 logger.setLevel(logger.levels.INFO); // Be sure to call setLevel method in order to apply plugin
 
 logger.info();
