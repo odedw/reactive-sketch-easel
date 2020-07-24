1f4d4847da5e3631f91c84cedd7d9a390b9c5b23
Oded Welgreen <oded.welgreen@gmail.com>
2020-07-23 22:53:33 -0500

    extract to scene class

--- web/src/sketches/Miscellaneous/KanizsaTriangle.sketch.ts
+++ web/src/sketches/Miscellaneous/KanizsaTriangle.sketch.ts
@@ -1,16 +1,15 @@
 import Sketch from '../Sketch';
-import { Vector } from 'p5';
+import p5, { Vector } from 'p5';
 
 const TRIANGLE_SIZE = 300;
 const ELLIPSE_SIZE = 100;
 const STROKE_SIZE = 5;
-export default class KanizsaTriangle extends Sketch {
+
+class Scene1 {
   visibleTriangle: Vector[] = [];
   hiddenTriangle: Vector[] = [];
-  setup() {
-    const p = this.p;
-    p.createCanvas(this.w, this.h);
-    p.rectMode(p.CENTER);
+
+  setup(p: p5) {
     let size = TRIANGLE_SIZE - STROKE_SIZE;
     let altitude = (size * Math.sqrt(3)) / 2;
 
@@ -23,22 +22,38 @@ export default class KanizsaTriangle extends Sketch {
     this.hiddenTriangle.push(p.createVector(-TRIANGLE_SIZE / 2, -altitude / 3));
     this.hiddenTriangle.push(p.createVector(+TRIANGLE_SIZE / 2, -altitude / 3));
   }
-  draw() {
-    const p = this.p;
-    p.background(255);
-    // p.rotate(p.frameCount * 0.01);
-    p.translate(this.center.x, this.center.y);
+
+  draw(p: p5) {
+    p.translate(p.width / 2, p.height / 2);
     p.fill(0).strokeWeight(0);
     p.ellipse(this.hiddenTriangle[0].x, this.hiddenTriangle[0].y, ELLIPSE_SIZE);
     p.ellipse(this.hiddenTriangle[1].x, this.hiddenTriangle[1].y, ELLIPSE_SIZE);
     p.ellipse(this.hiddenTriangle[2].x, this.hiddenTriangle[2].y, ELLIPSE_SIZE);
     p.fill(255).stroke(0).strokeWeight(STROKE_SIZE);
-    this.drawTriangle(this.visibleTriangle);
+    this.drawTriangle(p, this.visibleTriangle);
     p.fill(255).stroke(0).strokeWeight(0);
-    // p.rotate(p.frameCount * 0.005);
-    this.drawTriangle(this.hiddenTriangle);
+    p.push();
+    p.rotate(p.frameCount * 0.005);
+    this.drawTriangle(p, this.hiddenTriangle);
+    p.pop();
   }
-  drawTriangle(triangle: Vector[]) {
-    this.p.triangle(triangle[0].x, triangle[0].y, triangle[1].x, triangle[1].y, triangle[2].x, triangle[2].y);
+
+  drawTriangle(p: p5, triangle: Vector[]) {
+    p.triangle(triangle[0].x, triangle[0].y, triangle[1].x, triangle[1].y, triangle[2].x, triangle[2].y);
+  }
+}
+export default class KanizsaTriangle extends Sketch {
+  scene1 = new Scene1();
+  setup() {
+    const p = this.p;
+    p.createCanvas(this.w, this.h);
+    p.rectMode(p.CENTER);
+    this.scene1.setup(p);
+  }
+  draw() {
+    const p = this.p;
+    p.background(255);
+
+    this.scene1.draw(p);
   }
 }
