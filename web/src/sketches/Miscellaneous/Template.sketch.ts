b408ffc05264ce81c519a1c985bbbc624897b9cc
Oded Welgreen <oded.welgreen@gmail.com>
2020-08-22 15:49:19 -0500

    set framerate

--- web/src/sketches/Miscellaneous/Template.sketch.ts
+++ web/src/sketches/Miscellaneous/Template.sketch.ts
@@ -1,9 +1,10 @@
-import Sketch from "../Sketch";
+import Sketch from '../Sketch';
 
 export default class Template extends Sketch {
   setup() {
     const p = this.p;
     p.createCanvas(this.w, this.h);
+    p.frameRate(60);
   }
   draw() {
     const p = this.p;
