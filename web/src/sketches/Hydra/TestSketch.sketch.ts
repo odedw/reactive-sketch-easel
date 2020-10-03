c43ac274731e6db6f2f835cad9ccf7a3f6fa950c
Oded Welgreen <oded.welgreen@gmail.com>
2020-10-02 23:18:56 -0500

    add some types

--- web/src/sketches/Hydra/TestSketch.sketch.ts
+++ web/src/sketches/Hydra/TestSketch.sketch.ts
@@ -11,7 +11,7 @@ export default class TestSketch extends HydraSketch {
     // MidiEventEmitter.ccBind<MidiData>(51, 'bd', this.d, 0.01);
   }
   run() {
-    s0.initScreen();
-    src(s0).contrast(1).luma(0.1).brightness(0.01).scale(1.2, 1.2).repeat(3, 3).pixelate(300, 300).out();
+    // s0.initScreen();
+    // src(s0).contrast(1).luma(0.1).brightness(0.01).scale(1.2, 1.2).repeat(3, 3).pixelate(300, 300).out();
   }
 }
