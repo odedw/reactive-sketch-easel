bb20e0a3eed276c914ce969f702d13e974139ed3
Oded Welgreen <oded.welgreen@gmail.com>
2021-01-17 21:25:30 -0600

    genuary 4

--- web/src/utils/extensions.ts
+++ web/src/utils/extensions.ts
@@ -5,5 +5,25 @@ Array.prototype.random = function () {
 
 // eslint-disable-next-line
 Array.prototype.randomize = function () {
-  return this.sort(() => Math.random() - 0.5);
+  return shuffle(this);
 };
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
