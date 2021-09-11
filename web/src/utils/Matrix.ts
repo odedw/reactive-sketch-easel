ad2b24c9a0217b05f720bce6d941fef02adf35c3
Oded Welgreen <oded.welgreen@gmail.com>
2021-09-11 16:20:07 -0500

    finish sketch

---
+++ web/src/utils/Matrix.ts
@@ -0,0 +1,62 @@
+export class Matrix<T> {
+  w: number;
+  h: number;
+  values: T[][] = [];
+  size: number;
+  constructor(w: number, h: number, init: (i: number, j: number) => T) {
+    this.w = w;
+    this.h = h;
+    // this.values = Array(h).fill(Array(w).fill(zero));
+    for (let i = 0; i < h; i++) {
+      if (!this.values[i]) {
+        this.values[i] = [];
+      }
+      for (let j = 0; j < w; j++) {
+        this.values[i][j] = init(i, j);
+      }
+    }
+    this.size = w * h;
+  }
+
+  get(i: number, j: number): T {
+    return this.values[i][j];
+  }
+
+  set(i: number, j: number, value: T) {
+    this.values[i][j] = value;
+  }
+
+  randomIndex() {
+    return {
+      i: Math.floor(Math.random() * this.h),
+      j: Math.floor(Math.random() * this.w),
+    };
+  }
+
+  items() {
+    return this.values.flatMap((a) => a);
+  }
+
+  getNeighborIndices(i: number, j: number): { i: number; j: number }[] {
+    const result = [];
+    if (i - 1 >= 0) {
+      result.push({ i: i - 1, j });
+    }
+    if (i + 1 < this.h) {
+      result.push({ i: i + 1, j });
+    }
+    if (j - 1 >= 0) {
+      result.push({ i: i, j: j - 1 });
+    }
+    if (j + 1 < this.w) {
+      result.push({ i: i, j: j + 1 });
+    }
+    return result;
+  }
+
+  switch(i1: number, j1: number, i2: number, j2: number) {
+    const temp = this.values[i1][j1];
+    this.values[i1][j1] = this.values[i2][j2];
+    this.values[i2][j2] = temp;
+  }
+}
