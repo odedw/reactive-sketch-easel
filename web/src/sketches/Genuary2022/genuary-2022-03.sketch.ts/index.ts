17f7c461a98ca019a3afe0aa52ac20bf48368aa2
Oded Welgreen <oded.welgreen@gmail.com>
2022-01-11 14:30:51 -0600

    add trigger

--- web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
+++ web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
@@ -1,14 +1,16 @@
 import { Image } from 'p5';
-import ProcessingSketch from '../../ProcessingSketch';
+import MidiSketch from '../../P5/MidiSketch';
 import { CelestialObject } from './CelestialObject';
 import { Twinkle } from './Twinkle';
+import { Output } from '@reactive-sketch-easel/midi';
 
 const NUM_TWINKLES = 2000;
-export default class Template extends ProcessingSketch {
+export default class Template extends MidiSketch {
   celestialObjects: CelestialObject[] = [];
   twinkles: Twinkle[] = [];
+  output: Output;
 
-  sun;
+  sun: CelestialObject;
   jupiter: Image;
   images: Image[] = [];
   preload() {
@@ -21,93 +23,28 @@ export default class Template extends ProcessingSketch {
     this.images.push(this.p.loadImage('/assets/planets/saturn.jpg'));
     this.images.push(this.p.loadImage('/assets/planets/uranus.jpg'));
     this.images.push(this.p.loadImage('/assets/planets/neptune.jpg'));
+    Output.create('p5').then((o) => (this.output = o));
   }
   setup() {
+    this.onTrigger = this.onTrigger.bind(this);
     const p = this.p;
     p.angleMode(p.DEGREES);
     p.createCanvas(this.w, this.h, p.WEBGL);
     p.frameRate(60);
-    let planets: CelestialObject[] = [];
     // p.camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
-    const pixelsPerDistanceUnit = 70;
-    const speedUnit = 2;
-
-    planets = [
-      new CelestialObject(
-        p,
-        4,
-        Math.log(3.9) * pixelsPerDistanceUnit,
-        p.color('#B5A7A7'),
-        (1 / Math.log(88)) * speedUnit,
-        [],
-        this.images[1]
-      ), //mercury
-      new CelestialObject(
-        p,
-        8,
-        Math.log(7.2) * pixelsPerDistanceUnit,
-        p.color('#DDD8D4'),
-        (1 / Math.log(225)) * speedUnit,
-        [],
-        this.images[2]
-      ), //venus
-      new CelestialObject(
-        p,
-        9,
-        Math.log(10) * pixelsPerDistanceUnit,
-        p.color('#8CB1DE'),
-        (1 / Math.log(365)) * speedUnit,
-        [],
-        this.images[3]
-      ), //earth
-      new CelestialObject(
-        p,
-        6,
-        Math.log(15.2) * pixelsPerDistanceUnit,
-        p.color('#E27B58'),
-        (1 / Math.log(687)) * speedUnit,
-        [],
-        this.images[4]
-      ), //mars
-      new CelestialObject(
-        p,
-        15,
-        Math.log(52) * pixelsPerDistanceUnit,
-        p.color('#D39C7E'),
-        (1 / Math.log(4333)) * speedUnit,
-        [],
-        this.images[5]
-      ), //jupiter
-      new CelestialObject(
-        p,
-        14,
-        Math.log(95.4) * pixelsPerDistanceUnit,
-        p.color('#C5AB6E'),
-        (1 / Math.log(10759)) * speedUnit,
-        [],
-        this.images[6]
-      ), //saturn
-      new CelestialObject(
-        p,
-        10,
-        Math.log(192) * pixelsPerDistanceUnit,
-        p.color('#BBE1E4'),
-        (1 / Math.log(30687)) * speedUnit,
-        [],
-        this.images[7]
-      ), //uranus
-      new CelestialObject(
-        p,
-        10,
-        Math.log(300.6) * pixelsPerDistanceUnit,
-        p.color('#6081FF'),
-        (1 / Math.log(60190)) * speedUnit,
-        [],
-        this.images[8]
-      ), //neptune
+    const pd = 70;
+    const su = 2;
+    let planets = [
+      new CelestialObject(p, 4, Math.log(3.9) * pd, (1 / Math.log(88)) * su, this.images[1], this.onTrigger), //mercury
+      new CelestialObject(p, 8, Math.log(7.2) * pd, (1 / Math.log(225)) * su, this.images[2], this.onTrigger), //venus
+      new CelestialObject(p, 9, Math.log(10) * pd, (1 / Math.log(365)) * su, this.images[3], this.onTrigger), //earth
+      new CelestialObject(p, 6, Math.log(15.2) * pd, (1 / Math.log(687)) * su, this.images[4], this.onTrigger), //mars
+      new CelestialObject(p, 15, Math.log(52) * pd, (1 / Math.log(4333)) * su, this.images[5], this.onTrigger), //jupiter
+      new CelestialObject(p, 14, Math.log(95.4) * pd, (1 / Math.log(10759)) * su, this.images[6], this.onTrigger), //saturn
+      new CelestialObject(p, 10, Math.log(192) * pd, (1 / Math.log(30687)) * su, this.images[7], this.onTrigger), //uranus
+      new CelestialObject(p, 10, Math.log(300.6) * pd, (1 / Math.log(60190)) * su, this.images[8], this.onTrigger), //neptune
     ];
-    this.sun = new CelestialObject(p, 50, 0, p.color(252, 229, 112), 0, planets, this.images[0]);
-
+    this.sun = new CelestialObject(p, 50, 0, 0, this.images[0], this.onTrigger, planets);
     this.celestialObjects.push(this.sun);
     p.ellipseMode(p.RADIUS);
     for (let i = 0; i < NUM_TWINKLES; i++) {
@@ -129,5 +66,10 @@ export default class Template extends ProcessingSketch {
       co.update(p);
       co.draw(p);
     }
+  }
+
+  onTrigger(c: CelestialObject) {
+    if (c === this.sun) return;
+    const index = this.sun.moons.indexOf(c);
   }
 }
