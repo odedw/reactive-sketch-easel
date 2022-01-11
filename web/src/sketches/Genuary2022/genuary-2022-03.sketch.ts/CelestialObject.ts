369c11efd48bd33198c29c9b83d818206020b409
Oded Welgreen <oded.welgreen@gmail.com>
2022-01-10 22:34:27 -0600

    change to 3d

--- web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/CelestialObject.ts
+++ web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/CelestialObject.ts
@@ -7,61 +7,37 @@ export class CelestialObject {
   fill: p5.Color;
   angle: number;
   speed: number;
-  pg: p5.Graphics;
-  constructor(p: p5, r: number, d: number, f: Color, s: number, m: CelestialObject[] = []) {
+  img: p5.Image;
+
+  constructor(p: p5, r: number, d: number, f: Color, s: number, m: CelestialObject[] = [], img: p5.Image = null) {
     this.distance = d;
     this.radius = r;
     this.fill = f;
     this.moons = m;
     this.angle = p.random(0, 360);
     this.speed = s; //p.random(5, 20);
-    this.pg = p.createGraphics(2 * r, 2 * r);
-    // this.pg.rectMode(p.CENTER);
-    this.pg.noStroke();
-    this.pg.fill(this.fill);
-    // this.pg.rect(0, 0, this.pg.width, this.pg.height);
-    this.pg.circle(r, r, r * 2);
-    // let img = p.createImage(2 * r, 2 * r);
-    // img.loadPixels();
-
-    // for (let y = 0; y < img.height; y++) {
-    //   for (let x = 0; x < img.width; x++) {
-    //     let index = (x + y * img.width) * 4;
-    //     img.pixels[index] = p.red(this.fill);
-    //     img.pixels[index + 1] = p.green(this.fill);
-    //     img.pixels[index + 2] = p.blue(this.fill);
-    //     img.pixels[index + 3] = p.alpha(this.fill);
-    //   }
-    // }
-    // img.updatePixels();
-    // this.pg.image(img, 0, 0);
-    // this.pg.square(0, 0, this.radius * 2);
+    this.img = img;
   }
 
   update(p: p5) {
     this.moons.forEach((m) => m.update(p));
-    this.angle += this.speed;
+    const prevAngle = this.angle;
+    this.angle = (this.angle + this.speed) % 360;
+    if (this.angle > 270 && prevAngle < 270) {
+      console.log('pass');
+    }
   }
 
   draw(p: p5) {
     p.push();
-    p.rotate(this.angle);
-    p.translate(this.distance - this.radius, -this.radius);
-
-    // p.noStroke();
-    // p.fill(this.fill);
-    // p.ellipse(0, 0, this.radius);
-    // this.pg.loadPixels();
-    // for (let x = 0; x < this.pg.width; x++) {
-    //   for (let y = 0; y < this.pg.height; y++) {
-    //     const index = y * 4 * this.pg.width + x * 4 + 3;
-    //     this.pg.pixels[index] = p.noise(x / 100, y / 100, p.frameCount / 1000) * 100 + 155;
-    //   }
-    // }
-
-    // this.pg.updatePixels();
-    p.image(this.pg, 0, 0);
-    p.translate(this.radius, this.radius);
+    p.noStroke();
+    p.translate(this.distance * p.cos(this.angle), this.distance * p.sin(this.angle));
+    // p.translate(this.distance * p.cos(0), this.distance * p.sin(0));
+    p.texture(this.img);
+    p.push();
+    p.rotateY(p.frameCount);
+    p.sphere(this.radius);
+    p.pop();
 
     this.moons.forEach((m) => m.draw(p));
     p.pop();
--- web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
+++ web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
@@ -1,3 +1,4 @@
+import { Image } from 'p5';
 import ProcessingSketch from '../../ProcessingSketch';
 import { CelestialObject } from './CelestialObject';
 import { Twinkle } from './Twinkle';
@@ -8,28 +9,27 @@ export default class Template extends ProcessingSketch {
   twinkles: Twinkle[] = [];
 
   sun;
+  jupiter: Image;
+  images: Image[] = [];
+  preload() {
+    this.images.push(this.p.loadImage('/assets/planets/sun.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/mercury.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/venus.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/earth.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/mars.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/jupiter.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/saturn.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/uranus.jpg'));
+    this.images.push(this.p.loadImage('/assets/planets/neptune.jpg'));
+  }
   setup() {
     const p = this.p;
     p.angleMode(p.DEGREES);
-    p.createCanvas(this.w, this.h);
+    p.createCanvas(this.w, this.h, p.WEBGL);
     p.frameRate(60);
-    // debugger;
     let planets: CelestialObject[] = [];
-    // const numPlanets = 6; //p.int(p.random(3, 6));
-    // const maxD = p.height / 2;
-    let currD = 0;
-    // for (let i = 0; i < numPlanets; i++) {
-    //   // const distanceFromLastPlanet = p.random(50, 100);
-    //   const prevPlanet = planets[i - 1];
-    //   const s = !prevPlanet ? p.random(20, 50) : p.random(prevPlanet.speed / 2, prevPlanet.speed);
-    //   const d = !prevPlanet ? p.random(50, 100) : p.random(50, 100) + prevPlanet.distance + prevPlanet.radius;
-    //   currD += d;
-    //   const r = !prevPlanet ? p.random(3, 6) : p.random(5, 30);
-    //   const planet = new CelestialObject(p, r, d, p.color(p.random(0, 255), p.random(0, 255), p.random(0, 255)), s, []);
-    //   planets.push(planet);
-    // }
+    // p.camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
     const pixelsPerDistanceUnit = 70;
-    const sizeRatio = 1;
     const speedUnit = 2;
 
     planets = [
@@ -38,66 +38,81 @@ export default class Template extends ProcessingSketch {
         4,
         Math.log(3.9) * pixelsPerDistanceUnit,
         p.color('#B5A7A7'),
-        (1 / Math.log(88)) * speedUnit
+        (1 / Math.log(88)) * speedUnit,
+        [],
+        this.images[1]
       ), //mercury
       new CelestialObject(
         p,
         8,
         Math.log(7.2) * pixelsPerDistanceUnit,
         p.color('#DDD8D4'),
-        (1 / Math.log(225)) * speedUnit
+        (1 / Math.log(225)) * speedUnit,
+        [],
+        this.images[2]
       ), //venus
       new CelestialObject(
         p,
         9,
         Math.log(10) * pixelsPerDistanceUnit,
         p.color('#8CB1DE'),
-        (1 / Math.log(365)) * speedUnit
+        (1 / Math.log(365)) * speedUnit,
+        [],
+        this.images[3]
       ), //earth
       new CelestialObject(
         p,
         6,
         Math.log(15.2) * pixelsPerDistanceUnit,
         p.color('#E27B58'),
-        (1 / Math.log(687)) * speedUnit
+        (1 / Math.log(687)) * speedUnit,
+        [],
+        this.images[4]
       ), //mars
       new CelestialObject(
         p,
         15,
         Math.log(52) * pixelsPerDistanceUnit,
         p.color('#D39C7E'),
-        (1 / Math.log(4333)) * speedUnit
+        (1 / Math.log(4333)) * speedUnit,
+        [],
+        this.images[5]
       ), //jupiter
       new CelestialObject(
         p,
         14,
         Math.log(95.4) * pixelsPerDistanceUnit,
         p.color('#C5AB6E'),
-        (1 / Math.log(10759)) * speedUnit
+        (1 / Math.log(10759)) * speedUnit,
+        [],
+        this.images[6]
       ), //saturn
       new CelestialObject(
         p,
         10,
         Math.log(192) * pixelsPerDistanceUnit,
         p.color('#BBE1E4'),
-        (1 / Math.log(30687)) * speedUnit
+        (1 / Math.log(30687)) * speedUnit,
+        [],
+        this.images[7]
       ), //uranus
       new CelestialObject(
         p,
         10,
         Math.log(300.6) * pixelsPerDistanceUnit,
         p.color('#6081FF'),
-        (1 / Math.log(60190)) * speedUnit
+        (1 / Math.log(60190)) * speedUnit,
+        [],
+        this.images[8]
       ), //neptune
     ];
-    this.sun = new CelestialObject(p, 50, 0, p.color(252, 229, 112), 0, planets);
+    this.sun = new CelestialObject(p, 50, 0, p.color(252, 229, 112), 0, planets, this.images[0]);
 
     this.celestialObjects.push(this.sun);
     p.ellipseMode(p.RADIUS);
     for (let i = 0; i < NUM_TWINKLES; i++) {
       this.twinkles.push(new Twinkle(p));
     }
-    
   }
   draw() {
     const p = this.p;
@@ -108,7 +123,8 @@ export default class Template extends ProcessingSketch {
       t.draw(p);
     }
     p.pop();
-    p.translate(p.width / 2, p.height / 2);
+    p.lights();
+    // p.translate(p.width / 2, p.height / 2);
     for (const co of this.celestialObjects) {
       co.update(p);
       co.draw(p);
