a409b30fd83bca124be267f6cfc61fe0734d32c8
Oded Welgreen <oded.welgreen@gmail.com>
2022-01-12 22:51:58 -0600

    tweak notes

--- web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
+++ web/src/sketches/Genuary2022/genuary-2022-03.sketch.ts/index.ts
@@ -1,11 +1,11 @@
 import { Image } from 'p5';
-import MidiSketch from '../../P5/MidiSketch';
+import { Output } from 'rmidi';
+import ProcessingSketch from '../../ProcessingSketch';
 import { CelestialObject } from './CelestialObject';
 import { Twinkle } from './Twinkle';
-import { Output } from '@reactive-sketch-easel/midi';
 
 const NUM_TWINKLES = 2000;
-export default class Template extends MidiSketch {
+export default class Template extends ProcessingSketch {
   celestialObjects: CelestialObject[] = [];
   twinkles: Twinkle[] = [];
   output: Output;
@@ -23,7 +23,17 @@ export default class Template extends MidiSketch {
     this.images.push(this.p.loadImage('/assets/planets/saturn.jpg'));
     this.images.push(this.p.loadImage('/assets/planets/uranus.jpg'));
     this.images.push(this.p.loadImage('/assets/planets/neptune.jpg'));
-    Output.create('p5').then((o) => (this.output = o));
+    Output.create('p5')
+      .then((o) => {
+        console.log(`===========================initialized`);
+
+        this.output = o;
+      })
+      .catch((e) => {
+        console.log('===========================');
+        console.log(e);
+        console.log('===========================');
+      });
   }
   setup() {
     this.onTrigger = this.onTrigger.bind(this);
@@ -33,25 +43,25 @@ export default class Template extends MidiSketch {
     p.frameRate(60);
     // p.camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
     const pd = 70;
-    const su = 2;
+    const su = 3;
     let planets = [
       new CelestialObject(p, 4, Math.log(3.9) * pd, (1 / Math.log(88)) * su, this.images[1], this.onTrigger), //mercury
-      new CelestialObject(p, 8, Math.log(7.2) * pd, (1 / Math.log(225)) * su, this.images[2], this.onTrigger), //venus
-      new CelestialObject(p, 9, Math.log(10) * pd, (1 / Math.log(365)) * su, this.images[3], this.onTrigger), //earth
-      new CelestialObject(p, 6, Math.log(15.2) * pd, (1 / Math.log(687)) * su, this.images[4], this.onTrigger), //mars
-      new CelestialObject(p, 15, Math.log(52) * pd, (1 / Math.log(4333)) * su, this.images[5], this.onTrigger), //jupiter
+      new CelestialObject(p, 8, Math.log(6) * pd, (1 / Math.log(225)) * su, this.images[2], this.onTrigger), //venus
+      new CelestialObject(p, 9, Math.log(9) * pd, (1 / Math.log(365)) * su, this.images[3], this.onTrigger), //earth
+      new CelestialObject(p, 6, Math.log(14.2) * pd, (1 / Math.log(687)) * su, this.images[4], this.onTrigger), //mars
+      new CelestialObject(p, 15, Math.log(34) * pd, (1 / Math.log(4333)) * su, this.images[5], this.onTrigger), //jupiter
       new CelestialObject(
         p,
         14,
-        Math.log(95.4) * pd,
+        Math.log(80) * pd,
         (1 / Math.log(10759)) * su,
         this.images[6],
         this.onTrigger,
         [],
         true
       ), //saturn
-      new CelestialObject(p, 10, Math.log(192) * pd, (1 / Math.log(30687)) * su, this.images[7], this.onTrigger), //uranus
-      new CelestialObject(p, 10, Math.log(300.6) * pd, (1 / Math.log(60190)) * su, this.images[8], this.onTrigger), //neptune
+      new CelestialObject(p, 10, Math.log(180) * pd, (1 / Math.log(30687)) * su, this.images[7], this.onTrigger), //uranus
+      new CelestialObject(p, 10, Math.log(280) * pd, (1 / Math.log(60190)) * su, this.images[8], this.onTrigger), //neptune
     ];
     this.sun = new CelestialObject(p, 50, 0, 0, this.images[0], this.onTrigger, planets);
     this.celestialObjects.push(this.sun);
@@ -64,7 +74,7 @@ export default class Template extends MidiSketch {
     const p = this.p;
     p.background(0, 0, 0);
     p.push();
-    p.rotate(p.frameCount / 1000);
+    p.rotateZ(p.frameCount / 100);
     for (const t of this.twinkles) {
       t.draw(p);
     }
@@ -80,5 +90,40 @@ export default class Template extends MidiSketch {
   onTrigger(c: CelestialObject) {
     if (c === this.sun) return;
     const index = this.sun.moons.indexOf(c);
+    let note = '';
+    let duration = this.p.floor(this.p.random(5000, 10000));
+    let octave = this.p.floor(this.p.random(3, 5));
+    if (index === 0) {
+      //mercury
+      note = 'C';
+      duration = 4000;
+    } else if (index === 1) {
+      //venus
+      note = 'D';
+    } else if (index === 2) {
+      //earth
+      note = 'E';
+    } else if (index === 3) {
+      //mars
+      note = 'F#';
+    } else if (index === 4) {
+      //jupiter
+      note = 'G';
+    } else if (index === 5) {
+      //saturn
+      note = 'A';
+    } else if (index === 6) {
+      //uranus
+      note = 'B';
+    } else if (index === 7) {
+      //neptune
+      note = 'C';
+      octave = 2;
+      duration = 16000;
+    }
+    console.log(`${note} | ${index + 1} | ${duration}`);
+    this.output?.midiOutput.playNote(`${note}${octave.toString()}`, index + 1, {
+      duration,
+    });
   }
 }
