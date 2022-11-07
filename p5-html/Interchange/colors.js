4a8000bf87650bd7e724b8082a7316ade5a0b9bf
Oded Welgreen <oded.welgreen@gmail.com>
2022-11-06 21:26:09 -0600

    add colors script

---
+++ p5-html/Interchange/colors.js
@@ -0,0 +1,8 @@
+const palettes = [
+  ['#ff55ff', '#55ffff', '#000000', '#ffffff'],
+  ['#af92b5', '#8b7991', '#6f597a', '#624b6e', '#503d5c'],
+  ['#c7522a', '#e5c185', '#fbf2c4', '#74a892', '#008585'],
+  ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600'],
+  ['#801100', '#B62203', '#D73502', '#FC6400', '#FF7500', '#FAC000'],
+];
+const RANDOM_PALETTE = palettes[Math.floor(Math.random() * palettes.length)];
--- p5-html/Interchange/sketch.js
+++ p5-html/Interchange/sketch.js
@@ -1,26 +1,87 @@
 /// <reference path="../../node_modules/@types/p5/global.d.ts" />
-const WIDTH = 4;
-const GAP = WIDTH * 2;
+const SIZE = 8;
+const GAP = 8;
+const MIN_SEGMENT_SIZE = SIZE * 2;
+const MAX_SEGMENT_SIZE = SIZE * 4;
 
-const tracks = [];
+const frate = 30; // frame rate
+const numFrames = frate * 30; // num of frames to record
+let recording = false;
+let recordedFrames = 0;
+let cwidth = 540;
+let cheight = 540;
+let encoder;
+
+const vertical = [];
+const horizontal = [];
+// make sure encoder is ready before use
+function preload() {
+  HME.createH264MP4Encoder().then((enc) => {
+    encoder = enc;
+    encoder.outputFilename = 'test';
+    encoder.width = cwidth * pixelDensity();
+    encoder.height = cheight * pixelDensity();
+    encoder.frameRate = frate;
+    encoder.kbps = 50000; // video quality
+    encoder.groupOfPictures = 10; // lower if you have fast actions.
+    encoder.initialize();
+  });
+}
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(cwidth, cheight);
 
-  let x = random(-WIDTH, 0);
-  while (x < width) {
-    let c = lerpColor(color('red'), color('blue'), x / width);
-    const t = new Track(x, WIDTH, random(-0.5, 0.5), c);
-    tracks.push(t);
-    x += t.w + GAP;
+  let x = 0; //random(-SIZE, 0);
+  while (x < width + GAP) {
+    let c = random(RANDOM_PALETTE); // lerpColor(color('darkred'), color('blue'), x / width);
+    const t = new Track(x, SIZE, random(-1, 1), true, c);
+    vertical.push(t);
+    x += t.size + GAP;
   }
-  // tracks.push(new Track(width / 2, WIDTH, -0.5));
+  let y = 0;
+  while (y < height + GAP) {
+    let c = random(RANDOM_PALETTE); // lerpColor(color('darkred'), color('blue'), x / width);
+    const t = new Track(y, SIZE, random(-1, 1), false, c);
+    horizontal.push(t);
+    y += t.size + GAP;
+  }
+  // horizontal.push(new Track(width / 2, SIZE, 0.5, false, 'white'));
 }
 
 function draw() {
   background(0);
-
-  for (let t of tracks) {
+  stroke(0);
+  strokeWeight(1);
+  for (let t of vertical) {
     t.update();
     t.draw();
+  }
+  for (let t of horizontal) {
+    t.update();
+    t.draw();
+  }
+
+  // keep adding new frame
+  if (recording) {
+    encoder.addFrameRgba(drawingContext.getImageData(0, 0, encoder.width, encoder.height).data);
+    recordedFrames++;
+    if (recordedFrames % 100 === 0) {
+      console.log(`Recorded ${recordedFrames} of ${numFrames} frames`);
+    }
+  }
+  // finalize encoding and export as mp4
+  if (recordedFrames === numFrames) {
+    recording = false;
+    recordedFrames = 0;
+    console.log('recording stopped');
+
+    encoder.finalize();
+    const uint8Array = encoder.FS.readFile(encoder.outputFilename);
+    const anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(new Blob([uint8Array], { type: 'video/mp4' }));
+    anchor.download = encoder.outputFilename;
+    anchor.click();
+    encoder.delete();
+    noLoop();
   }
 }
