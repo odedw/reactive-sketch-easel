31981b6904472585be1959e5f6379b0e40715e37
Oded Welgreen <oded.welgreen@gmail.com>
2022-11-22 21:37:33 -0600

    update template

--- p5-html/SquareTemplate/sketch.js
+++ p5-html/SquareTemplate/sketch.js
@@ -1,7 +1,7 @@
 /// <reference path="../../node_modules/@types/p5/global.d.ts" />
 const WIDTH = 540;
 const HEIGHT = 540;
-const SHOULD_RECORD = true;
+const SHOULD_RECORD = false;
 const RECORD_TIME = 1;
 const FPS = 30;
 
