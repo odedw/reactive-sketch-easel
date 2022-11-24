02a5434cb62c2fcd8990a7f67e36e284174341e9
Oded Welgreen <oded.welgreen@gmail.com>
2022-11-24 00:17:30 -0600

    randomize

--- p5-html/2022-11-22_FadingLines/sketch.js
+++ p5-html/2022-11-22_FadingLines/sketch.js
@@ -1,18 +1,23 @@
 /// <reference path="../../node_modules/@types/p5/global.d.ts" />
-const NUM_PARTICLES = 50;
-const MAX_DISTANCE = 100;
-const STROKE_WEIGHT = 5;
 
+///////////////////////////////////////////
+let NUM_PARTICLES, MAX_DISTANCE, STROKE_WEIGHT, MAX_SPEED;
+// print(`NUM_PARTICLES: ${NUM_PARTICLES}`);
+// print(`MAX_DISTANCE: ${NUM_PARTICLES}`);
+// print(`STROKE_WEIGHT: ${NUM_PARTICLES}`);
+// print(`MAX_SPEED: ${NUM_PARTICLES}`);
+///////////////////////////////////////////
 const WIDTH = 540;
 const HEIGHT = 540;
 const SHOULD_RECORD = false;
 const RECORD_TIME = 1;
-const FPS = 24;
+const FPS = 60;
+let opacity = 10;
 
 class Particle {
   constructor() {
     this.pos = { x: random(width), y: random(height) };
-    this.vel = { x: random(1, 5), y: random(1, 5) };
+    this.vel = { x: random(0, MAX_SPEED), y: random(0, MAX_SPEED) };
   }
 
   update() {
@@ -29,13 +34,21 @@ class Particle {
 }
 
 let particles = [];
-let opacity;
 function preload() {
   preloadRecorder();
 }
 
 function setup() {
   createCanvas(540, 540);
+  NUM_PARTICLES = floor(random(30, 70));
+  MAX_DISTANCE = floor(random(50, 200));
+  STROKE_WEIGHT = floor(random(3, 8));
+  MAX_SPEED = floor(random(1.2, 1.8));
+  print(`NUM_PARTICLES: ${NUM_PARTICLES}`);
+  print(`MAX_DISTANCE: ${NUM_PARTICLES}`);
+  print(`STROKE_WEIGHT: ${NUM_PARTICLES}`);
+  print(`MAX_SPEED: ${NUM_PARTICLES}`);
+  ///////////////////////////////////////////
   frameRate(FPS);
   for (let i = 0; i < NUM_PARTICLES; i++) {
     particles.push(new Particle());
@@ -45,9 +58,9 @@ function setup() {
 }
 
 function draw() {
-  const alpha = map(sin(frameCount / 20), -1, 1, 20, 100);
+  // const alpha = map(sin(frameCount / 20), -1, 1, 20, 100);
   // console.log(alpha);
-  background(0, 0, 0, alpha);
+  background(0, 0, 0, opacity);
   for (let p of particles) {
     p.update();
     p.draw();
@@ -60,7 +73,7 @@ function draw() {
       let p2 = particles[j];
       const d = dist(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y);
       if (d < MAX_DISTANCE) {
-        const c = lerpColor(color('red'), color('blue'), d / MAX_DISTANCE);
+        const c = lerpColor(color('#00d8ff'), color('#008cff'), d / MAX_DISTANCE);
         stroke(c);
         line(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y);
       }
@@ -72,4 +85,13 @@ function draw() {
 
 function mouseClicked(event) {
   console.log('frameCount', frameCount);
+}
+
+function mouseWheel(event) {
+  //move the square according to the vertical scroll amount
+  opacity += Math.sign(event.delta) * -1;
+  print(opacity);
+
+  //uncomment to block page scrolling
+  //return false;
 }
