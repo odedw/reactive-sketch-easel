5ad9a5ea32c2d38dd2690d17f365436a6b49d1f5
Oded Welgreen <oded.welgreen@gmail.com>
2023-01-06 14:02:49 -0600

    switch to random segments

--- p5-html/2023-01-05_Genuary3-PixelSort/src/sketch.ts
+++ p5-html/2023-01-05_Genuary3-PixelSort/src/sketch.ts
@@ -1,14 +1,20 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 import { Graphics, Image, Vector } from 'p5';
-import { Modulate } from '../../utils/p5.modulate';
+import { Lfo, Modulate } from '../../utils/p5.modulate';
 
-const IMAGE_NAME = 'photo-1567309676325-2b237f42861f.jpg';
-
+const IMAGE_NAME = 'alexander-andrews-yOIT88xWkbg-unsplash.jpg';
+/*
+photo-1567309676325-2b237f42861f.jpg
+alexander-andrews-yOIT88xWkbg-unsplash.jpg
+erik-jan-leusink-IcW2ooxn4N4-unsplash.jpg
+jason-leung-UIZQfEZ1wUc-unsplash.jpg
+michele-caliani-iLAAT1E-H_8-unsplash.jpg
+pdfamousartists8-pdfamouspaintingetc10100013-image_2.jpg
+*/
 const WIDTH = 540;
 const HEIGHT = 540;
-const SEGMENT_SIZE = 10;
-
-let col = 0;
+const MIN_SEGMENT_SIZE = 10;
+const MAX_SEGMENT_SIZE = 50;
 let img: Image;
 let pg: Graphics;
 function preload() {
@@ -17,7 +23,7 @@ function preload() {
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  colorMode(HSB, 255);
+  // colorMode(HSB, 255);
   pixelDensity(1);
   frameRate(60);
   noStroke();
@@ -33,6 +39,13 @@ function setup() {
   //     pg.pixels[index + 3] = 255;
   //   }
   // }
+  // for (let i = 0; i < pg.pixels.length; i += 4) {
+  //   // const val = random(255);
+  //   pg.pixels[i] = random() < 0.5 ? 255 : 0;
+  //   pg.pixels[i + 1] = 0;
+  //   pg.pixels[i + 2] = 0; //random(255);
+  //   pg.pixels[i + 3] = 255;
+  // }
   // pg.updatePixels();
 }
 
@@ -42,50 +55,45 @@ function pixelIndex(x: number, y: number): number {
 }
 
 function sortPixels(pixels: Vector[]) {
-  console.log(pixels.length);
   pixels.sort((a, b) => {
     return brightness(color(a.x, a.y, a.z)) - brightness(color(b.x, b.y, b.z)); //(b.x + b.y, b.z) / 3 - (a.x + a.y + a.z) / 3;
   });
 }
 
-function processColumn(x: number) {
-  let y = 0;
-  while (y < height) {
-    let pixels = [];
-    let size = min(SEGMENT_SIZE, height - y);
-    for (let j = y; j < y + size; j++) {
-      pixels.push(
-        new Vector(pg.pixels[pixelIndex(x, j)], pg.pixels[pixelIndex(x, j)] + 1, pg.pixels[pixelIndex(x, j) + 2])
-      );
-    }
-    sortPixels(pixels);
-    for (let i = 0; i < pixels.length; i++) {
-      pg.pixels[pixelIndex(x, y + i)] = pixels[i].x;
-      pg.pixels[pixelIndex(x, y + i) + 1] = pixels[i].y;
-      pg.pixels[pixelIndex(x, y + i) + 2] = pixels[i].z;
-    }
-    y += size;
+function sortSegment(x: number, y: number, size: number, row: boolean) {
+  let segment = [];
+  const mainAxis = row ? x : y;
+  for (let i = mainAxis; i < mainAxis + size; i++) {
+    const index = pixelIndex(row ? i : x, row ? y : i);
+    segment.push(new Vector(pg.pixels[index], pg.pixels[index + 1], pg.pixels[index + 2]));
+  }
+  sortPixels(segment);
+  for (let i = 0; i < segment.length; i++) {
+    const index = pixelIndex(x + (row ? i : 0), y + (row ? 0 : i));
+    pg.pixels[index] = segment[i].x;
+    pg.pixels[index + 1] = segment[i].y;
+    pg.pixels[index + 2] = segment[i].z;
   }
 }
-function draw() {
-  // if (frameCount % 10 !== 0) return;
-  pg.loadPixels();
-  processColumn(col);
-  pg.updatePixels();
-  image(pg, 0, 0, width, height);
-  col++;
-  if (col === width) {
-    console.log('done');
 
-    noLoop();
+function draw() {
+  if (frameCount >= 60 && frameCount < 1000) {
+    pg.loadPixels();
+    for (let i = 0; i < 50; i++) {
+      const x = floor(random(width));
+      const y = floor(random(height));
+      const row = random() < 0.5;
+      let size = min(random(MIN_SEGMENT_SIZE, MAX_SEGMENT_SIZE), row ? width - x : height - y);
+      sortSegment(x, y, size, row);
+    }
+    pg.updatePixels();
   }
+  image(pg, 0, 0, width, height);
 }
 
 function mouseClicked(event?: object) {
   console.log('frameCount', frameCount);
-  pg.loadPixels();
-  processColumn(mouseX);
-  pg.updatePixels();
+  noLoop();
 }
 
 //#region add globals
